52bdd903fd44c6c9b00b719b7c1339e2abf235e7
tina <tinaw425@gmail.com>
2018-11-01 19:06:59 -0400

    added sample output

--- main.c
+++ main.c
@@ -14,11 +14,31 @@ int main() {
   // printf("Time of last access: %s", ctime(&(s->st_atime)));
 
   // free(s);
-
-  DIR * d = opendir("test");
+  DIR *d = opendir(".");
+  printf("Statistics for directory: . \n");
+  int size;
   struct dirent *entry;
-  entry = readdir(d);
-  printf("%s\n", entry->d_name);
+  struct stat *s = malloc(sizeof(struct stat));
+  while( (entry = readdir(d)) != NULL ) {
+    stat(entry->d_name, s);
+    size += s->st_size;
+  }
   closedir(d);
+  printf("Total Directory Size:%d\n", size);
+  d = opendir(".");
+  printf("Directories:\n");
+  while( (entry = readdir(d)) != NULL ) {
+    if(entry->d_type == 4) {
+      printf("%s\n", entry->d_name);
+    }
+  }
+  closedir(d);
+  d = opendir(".");
+  printf("Regular files:\n");
+  while( (entry = readdir(d)) != NULL ) {
+      if(entry->d_type == 8) {
+        printf("%s\n", entry->d_name);
+      }
+  }
   return 0;
 }
