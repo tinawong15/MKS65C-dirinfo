19ad903bcd48889774680319219218e77183a0d4
tina <tinaw425@gmail.com>
2018-11-01 19:43:01 -0400

    deleted old code

--- main.c
+++ main.c
@@ -1,18 +1,6 @@
 #include "dir.h"
 
 int main() {
-  // struct stat *s = malloc(sizeof(struct stat));
-  // stat("text.txt", s);
-  // printf("File Size: %lld\n", s->st_size);
-  // char size[256];
-  // int byte = s->st_size;
-  // int kb = byte / 1000;
-  // int mb = kb / 1000;
-  // int gb = mb / 1000;
-  // sprintf(size, "%d B\n%d KB\n%d MB\n%d GB", byte, kb, mb, gb);
-  // printf("%s\n", size);
-  // printf("Time of last access: %s", ctime(&(s->st_atime)));
-
   DIR *d = opendir(".");
   printf("Statistics for directory: . \n");
   int size;
